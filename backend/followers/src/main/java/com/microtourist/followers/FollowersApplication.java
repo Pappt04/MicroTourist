95649e9fc1e86b73cda27764fe50e62048b2d34c
Sofija <sofimihajlovic7@gmail.com>
2026-04-09 12:54:10 +0200

    Followers microservice

---
+++ backend/followers/src/main/java/com/microtourist/followers/FollowersApplication.java
@@ -0,0 +1,12 @@
+package com.microtourist.followers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowersApplication.class, args);
+    }
+}
